1d246013c1bdac8141465687fc7684168ece668a
kierancyphus <kieran.cyphus@mail.utoronto.ca>
2020-04-17 10:18:09 +0800

    Updates

---
+++ precache-manifest.957b1339ecf227275dafdcf92c7b7db2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811cecc5ba2156025eff91b6efb973d8",
+    "url": "/english_test/index.html"
+  },
+  {
+    "revision": "354db4d773fa85f03a92",
+    "url": "/english_test/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c06d6e649dbd3940c02f",
+    "url": "/english_test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "354db4d773fa85f03a92",
+    "url": "/english_test/static/js/2.3f82e3e5.chunk.js"
+  },
+  {
+    "revision": "946acdb7150413d91f733d9673168aa3",
+    "url": "/english_test/static/js/2.3f82e3e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c06d6e649dbd3940c02f",
+    "url": "/english_test/static/js/main.6bbf4045.chunk.js"
+  },
+  {
+    "revision": "41c792b23a8631927a55",
+    "url": "/english_test/static/js/runtime-main.c5bf06d7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/english_test/precache-manifest.43a28216b59544d8ab8137b6c089784a.js"
+  "/english_test/precache-manifest.957b1339ecf227275dafdcf92c7b7db2.js"
 );
 
 self.addEventListener('message', (event) => {
