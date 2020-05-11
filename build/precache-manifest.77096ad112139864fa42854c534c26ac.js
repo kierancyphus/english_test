0f7874bfb41a195d920272efe4899393403b6137
Alex Cyphus <alexjcyphus@gmail.com>
2020-05-11 14:12:26 -0700

    testing

---
+++ build/precache-manifest.77096ad112139864fa42854c534c26ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556205df3357a33cb2686b7531b5fb18",
+    "url": "/english_test/index.html"
+  },
+  {
+    "revision": "d06ef5e33fa862a973db",
+    "url": "/english_test/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "25d2caa797fd4e1976ca",
+    "url": "/english_test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d06ef5e33fa862a973db",
+    "url": "/english_test/static/js/2.91c427e7.chunk.js"
+  },
+  {
+    "revision": "946acdb7150413d91f733d9673168aa3",
+    "url": "/english_test/static/js/2.91c427e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d2caa797fd4e1976ca",
+    "url": "/english_test/static/js/main.36d9e4ed.chunk.js"
+  },
+  {
+    "revision": "98d26c6bfe4ba6c850dd",
+    "url": "/english_test/static/js/runtime-main.60b6f87d.js"
+  }
+]);
