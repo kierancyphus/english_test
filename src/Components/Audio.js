13bfbd2c0bcf7d8b076c908b4600aa29ac5db7e3
Alex Cyphus <alexjcyphus@gmail.com>
2020-05-27 15:33:22 +0200

    Update Audio.js

--- src/Components/Audio.js
+++ src/Components/Audio.js
@@ -20,14 +20,17 @@ class Audio extends React.Component {
         </div>
         <div className="content">
 
-
+        <h3><u>Questions 29 to 35 (one audio)</u></h3>
+        <p>Please answer the multiple choice comprehension questions based on this audio. You can listen to the audio twice.</p>
+        <OnlyPlayPauseButton src={"https://raw.githubusercontent.com/kierancyphus/english_test/master/" + this.test + ".mp4"} test={this.test}/>
+        <hr/>
           <div className="questions" Style="padding: 10px 0 10px 0">
-          <h3><u>Questions 29 to 39</u></h3>
+          <h3><u>Questions 35 to 45</u></h3>
           <p>Please fill in the missing word on the exam. You can only listen to each audio once.</p>
           <hr/>
             {questions.map(question =>
               <div key={"div" + question + this.test}>
-                <h4 key={"title" + question + this.test}>Question {question}</h4>
+                <h4 key={"title" + question + this.test}>Question {6+question}</h4>
                 <OnlyPlayButton src={"https://raw.githubusercontent.com/kierancyphus/english_test/master/" + this.test + "-audio-" + question.toString() + ".m4a"}
                                 key={"button" + question + this.test}
                                 question={question}
@@ -37,10 +40,7 @@ class Audio extends React.Component {
               </div>
             )}
           </div>
-          <h3><u>Questions 40 to 45 (one audio)</u></h3>
-          <p>Please answer the multiple choice comprehension questions based on this audio. You can listen to the audio twice.</p>
-          <OnlyPlayPauseButton src={"https://raw.githubusercontent.com/kierancyphus/english_test/master/" + this.test + ".mp4"} test={this.test}/>
-          <hr/>
+
         </div>
       </div>
     );
